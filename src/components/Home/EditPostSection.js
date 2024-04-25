bebaf9024938fdd9c1e4295202cc6b0e9b474899
gitpremsagar <psagar172@gmail.com>
2024-04-25 21:18:11 +0530

    feat - Add EditPostSection component and update Post component to handle editing

--- src/components/Home/EditPostSection.js
+++ src/components/Home/EditPostSection.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function CreatePostSection({ setIsEditing }) {
+export default function EditPostSection({ setIsEditing, title, content }) {
   return (
     <section className="">
-      <h2 className="text-2xl font-bold text-center my-10 ">Create Post</h2>
+      <h2 className="text-2xl font-bold text-center my-10 ">Edit Post</h2>
       <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-8 border-2 border-gray-200 ">
         <div className="">
           <form className="flex flex-col items-center">
@@ -11,10 +11,12 @@ export default function CreatePostSection({ setIsEditing }) {
               type="text"
               placeholder="Title"
               className="border border-gray-300 rounded-md p-2 m-2 w-full"
+              defaultValue={title}
             />
             <textarea
               placeholder="Content"
               className="border border-gray-300 rounded-md p-2 m-2 w-full"
+              defaultValue={content}
               rows={5}
             ></textarea>
 
@@ -25,7 +27,7 @@ export default function CreatePostSection({ setIsEditing }) {
               }}
               className="bg-blue-500 text-white rounded-md m-2 py-2 px-3"
             >
-              Post
+              Save
             </button>
           </form>
         </div>
--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { useState } from "react";
+import EditPostSection from "./EditPostSection";
 
 export default function Post({ post }) {
-  return (
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = (e) => {
+    e.preventDefault();
+    setIsEditing(true);
+  };
+  const handleDeleteClick = () => {
+    alert("Delete Clicked");
+  };
+
+  //   return (
+  //     <article className="max-w-md mx-auto bg-white rounded-xl border-2 border-gray-200 hover:border-gray-300 shadow-md hover:shadow-2xl overflow-hidden md:max-w-2xl m-10 transform hover:scale-110 transition ease-in-out duration-500">
+  //       <div className="p-8">
+  //         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+  //           Post
+  //         </div>
+  //         <a
+  //           href="#"
+  //           className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+  //         >
+  //           {post.title}
+  //         </a>
+  //         <p className="mt-2 text-gray-500">{post.content}</p>
+  //         <div>
+  //           <button
+  //             onClick={handleEditClick}
+  //             className="mt-4 mr-4 px-4 py-2 hover:text-white bg-white border border-indigo-600 hover:bg-indigo-800 transition ease-in-out rounded duration-500 "
+  //           >
+  //             Edit
+  //           </button>
+  //           <button
+  //             onClick={handleDeleteClick}
+  //             className="mt-4 mr-4 px-4 py-2 hover:text-white bg-white border border-red-600 hover:bg-red-800 transition ease-in-out duration-500 rounded "
+  //           >
+  //             Delete
+  //           </button>
+  //         </div>
+  //       </div>
+  //     </article>
+  //   );
+
+  return isEditing ? (
+    <EditPostSection
+      key={`write`}
+      setIsEditing={setIsEditing}
+      title={post.title}
+      content={post.content}
+    />
+  ) : (
     <article className="max-w-md mx-auto bg-white rounded-xl border-2 border-gray-200 hover:border-gray-300 shadow-md hover:shadow-2xl overflow-hidden md:max-w-2xl m-10 transform hover:scale-110 transition ease-in-out duration-500">
       <div className="p-8">
         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
@@ -15,10 +65,16 @@ export default function Post({ post }) {
         </a>
         <p className="mt-2 text-gray-500">{post.content}</p>
         <div>
-          <button className="mt-4 mr-4 px-4 py-2 hover:text-white bg-white border border-indigo-600 hover:bg-indigo-800 transition ease-in-out rounded duration-500 ">
+          <button
+            onClick={handleEditClick}
+            className="mt-4 mr-4 px-4 py-2 hover:text-white bg-white border border-indigo-600 hover:bg-indigo-800 transition ease-in-out rounded duration-500 "
+          >
             Edit
           </button>
-          <button className="mt-4 mr-4 px-4 py-2 hover:text-white bg-white border border-red-600 hover:bg-red-800 transition ease-in-out duration-500 rounded ">
+          <button
+            onClick={handleDeleteClick}
+            className="mt-4 mr-4 px-4 py-2 hover:text-white bg-white border border-red-600 hover:bg-red-800 transition ease-in-out duration-500 rounded "
+          >
             Delete
           </button>
         </div>
